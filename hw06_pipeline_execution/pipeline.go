75c8f147ed8a65fd71073254cf9715b5ace4b223
andrey.ivanov <andrey.ivanov@gs-labs.ru>
2025-04-05 00:01:04 +0300

    done hw06_pipeline_execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,9 @@
 package hw06pipelineexecution
 
+import (
+	"context"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +13,57 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		<-done
+		cancel()
+	}()
+	ch := in
+	for _, stage := range stages {
+		ch = wrapStage(ctx, stage, ch)
+	}
+
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		defer cancel()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case v, ok := <-ch:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+	return out
+}
+
+func wrapStage(ctx context.Context, stage Stage, in In) Out {
+	out := make(Bi)
+	go func() {
+		stageOut := stage(in)
+		defer func() {
+			close(out)
+			//nolint
+			for range stageOut {
+			}
+		}()
+		for v := range stageOut {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- v:
+			}
+		}
+	}()
+	return out
 }
